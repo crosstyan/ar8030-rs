aee69d617c22ce86c3d33bd3fc6a03b658188644
crosstyan <crosstyan@outlook.com>
2024-06-26 15:26:55 +0800

    first step

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), anyhow::Error> {
+    let devs = rusb::devices()?;
+    for dev in devs.iter() {
+        let desc = dev.device_descriptor()?;
+        println!("Bus {:03} Device {:03} ID {:04x}:{:04x}",
+                 dev.bus_number(), dev.address(),
+                 desc.vendor_id(), desc.product_id());
+    }
+    return Ok(());
 }
