e44828425509379756c4ab37a0aed3a724d6b8b7
crosstyan <crosstyan@outlook.com>
2024-06-27 15:37:36 +0800

    basic info

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,57 @@
 // Interestingly, it's registered as "Linux Foundation"
 use rusb::{Device, DeviceHandle, UsbContext};
 use log::{info, warn};
+// more on kv logging
+// https://docs.rs/log/latest/log/kv/index.html
 const ARTO_RTOS_VID: u16 = 0x1d6b;
 const ARTO_RTOS_PID: u16 = 0x8030;
 
-fn handle_ar8030<T>(dev: &Device<T>) -> Result<(), anyhow::Error>
+fn print_info<T>(dev: &Device<T>) -> Result<(), anyhow::Error>
 where
     T: UsbContext,
 {
-    let desc = dev.device_descriptor()?;
+    let dev_desc = dev.device_descriptor()?;
+    // https://libusb.sourceforge.io/api-1.0/
     info!(bus=dev.bus_number(),
           address=dev.address(),
-          vid=format!("0x{:04x}", desc.vendor_id()),
-          pid=format!("0x{:04x}", desc.product_id());
-          "AR8030 info");
+          port=dev.port_number(),
+          speed:?=dev.speed(),
+          vid=format!("0x{:04x}", dev_desc.vendor_id()),
+          pid=format!("0x{:04x}", dev_desc.product_id());
+          "device");
+
+    let config_desc = dev.active_config_descriptor()?;
+    let ifaces = config_desc.interfaces();
+    for iface in ifaces {
+        let iface_descs = iface.descriptors();
+        for iface_desc in iface_descs {
+            info!(iface=iface.number(),
+                  class=iface_desc.class_code(),
+                  subclass=iface_desc.sub_class_code(),
+                  protocol_code=iface_desc.protocol_code(),
+                  length=iface_desc.length();
+                  "interface");
+            let endpoints = iface_desc.endpoint_descriptors();
+            for endpoint in endpoints {
+                info!(iface=iface.number(),
+                      endpoint=endpoint.number(),
+                      direction:?=endpoint.direction(),
+                      address=endpoint.address(),
+                      interval=endpoint.interval(),
+                      length=endpoint.length(),
+                      max_packet_size=endpoint.max_packet_size();
+                      "endpoint");
+            }
+        }
+    }
+    let hdl = dev.open()?;
+    let manufacturer = hdl.read_manufacturer_string_ascii(&dev_desc).unwrap_or_default();
+    let product = hdl.read_product_string_ascii(&dev_desc).unwrap_or_default();
+    let serial = hdl.read_serial_number_string_ascii(&dev_desc).unwrap_or_default();
+    info!(manufacturer=manufacturer,
+          serial=serial,
+          product=product;
+          "string descriptors");
     Ok(())
 }
 
@@ -30,7 +68,7 @@ fn main() -> Result<(), anyhow::Error> {
         let vid = desc.vendor_id();
         let pid = desc.product_id();
         if vid == ARTO_RTOS_VID && pid == ARTO_RTOS_PID {
-            handle_ar8030(&dev)?;
+            print_info(&dev)?;
         }
     }
     return Ok(());
